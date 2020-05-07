9483a345b4176a9edcf1068e3fbb6a0a2b65c8d5
Mariana <mariana.diaz@live.com>
2020-05-07 02:26:24 -0400

    modify public url path, add homepage to package.json file

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,23 +1,10 @@
 import axios from "axios";
 require("dotenv").config();
-var keys = require("../keys.js");
-console.log(JSON.stringify(keys));
-
-var apiKey ="&key=AIzaSyDRKb2uixARbYa3BVCx87IC6CRPZ9t2Vnw";/*keys.googlebooks*/ 
-var url = "https://www.googleapis.com/books/v1/volumes/?q=";
-console.log(apiKey);
-
-
 
 export default {
-  // search: function (query) {
-  //   return axios.get(url + query + apiKey);
-  // },
-  // Gets all books
   getBooks: function (data) {
-    return axios.get(url);
-    //console.log(data);
-    //return axios.get("/api/books");
+    console.log(data);
+    return axios.get("/api/books");
   },
   
   // Deletes the book with the given id
